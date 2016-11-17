01a625316e88150943a2eb19d9c609f135fad68b
王蕾 <wanglei@wangleideMacBook-Pro.local>
2016-11-17 11:14:11 +0800

    Initial Commit

---
+++ cellViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cellViewTest
+//
+//  Created by 王蕾 on 2016/11/17.
+//  Copyright © 2016年 王蕾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cellViewTestTests/cellViewTestTests.m
@@ -0,0 +1,39 @@
+//
+//  cellViewTestTests.m
+//  cellViewTestTests
+//
+//  Created by 王蕾 on 2016/11/17.
+//  Copyright © 2016年 王蕾. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cellViewTestTests : XCTestCase
+
+@end
+
+@implementation cellViewTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
